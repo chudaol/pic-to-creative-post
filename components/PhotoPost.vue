133f4dddc3455c13472979cfcfed6da699f20650
Olga Filipova <chudaol@gmail.com>
2019-11-02 21:34:29 +0100

    created first structure

---
+++ components/PhotoPost.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card>
+    <v-img
+      class="white--text align-end"
+      :src="original ? originalUrl : creativeUrl"
+    >
+    </v-img>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn
+        @click="toggleOriginal"
+        outlined
+      >
+        {{original ? 'See creative post' : 'See original'}}
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+  export default {
+    props: ['originalUrl', 'creativeUrl'],
+    data () {
+      return {
+        original: true
+      }
+    },
+    methods: {
+      toggleOriginal () {
+        this.original = !this.original
+      }
+    }
+  }
+</script>
---
+++ store/state.js
@@ -0,0 +1,9 @@
+const pics = [
+  {
+    originalUrl: 'https://scontent.fmuc3-1.fna.fbcdn.net/v/t1.0-9/75539954_1870668146396937_2274021648057237504_n.jpg?_nc_cat=105&_nc_oc=AQnbWXICTmIPRh2sc_Alz6aZEu4cskdAfuPOPuXZ1JgWNm1xEidPZG0TDtmZDc26C8U&_nc_ht=scontent.fmuc3-1.fna&oh=7a2d3570633b41d02a45399b529dd110&oe=5E534998',
+    creativeUrl: 'https://scontent.fmuc3-1.fna.fbcdn.net/v/t1.0-0/p370x247/73177173_1870669773063441_4276603622160072704_n.jpg?_nc_cat=110&_nc_oc=AQntkc_Dmd6Ll6My0VergQtcjPJ4yCtyq3sk0WKfBf3EB0wxbMrvlIg3RFsCR6rJXO0&_nc_ht=scontent.fmuc3-1.fna&oh=3d568da889d8bc4279cf7120d2f0d0ae&oe=5E4CEE1E'
+  }
+]
+export default {
+  pics
+}
